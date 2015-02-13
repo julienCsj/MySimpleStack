bbd781a6eb2342ea8bc2e133d90a279fae861925
Julien Custoja <julien.custoja@gmail.com>
2015-02-13 12:13:04 +0100

    First commit

---
+++ src/main/java/dcll/jcus/App.java
@@ -0,0 +1,13 @@
+package dcll.jcus;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
